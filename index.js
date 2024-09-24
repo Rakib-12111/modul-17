63e975909bf1ebc44d4079ef64cd0ef8a864439f
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-24 14:34:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// var car = "cre is red"
+// console.log(car);
+// car = ' car id blue'
+// console.log(car);
+
+
+// const car = 'car id red'
+// console.log(car);
+// car = ' car is blue'
+// console.log(car);
+
+
+// var num = "Banglasehs"
+// console.log(typeof num);
+
+// var sum = true;
+// console.log(typeof sum);
+
+
+
+
+// var first = 0.1;
+// var second = 0.2;
+// var total = first + second;
+// console.log(parseFloat(total.toFixed(2)));
+var num1 = 40;
+var num2 = 2;
+var result = num1 % num2;
+console.log(result);
---
+++ shorthand.js
@@ -0,0 +1,12 @@
+// short cut rules
+
+
+// var price = 35;
+// //price = price + 10;
+// price += 10;
+// console.log(price);
+
+// var fiidge = 35000;
+// fiidge -= 1000;
+// console.log(fiidge);
+
